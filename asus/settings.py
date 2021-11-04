fd047247b1d0cc951fac75501b87f3ef9156c23e
asus <asuslogisticssocial@gmail.com>
2021-11-04 12:12:15 +0000

    minor changes

--- asus/settings.py
+++ asus/settings.py
@@ -12,20 +12,15 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
-import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-#initialise environment variable
-
-env = environ.Env()
-environ.Env.read_env()
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET')
+SECRET_KEY =('SECRET')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
