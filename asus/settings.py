6e0091c565995f1515f0f31e47751514c09379b6
asus <asuslogisticssocial@gmail.com>
2021-11-04 12:02:56 +0000

    minor changes

--- asus/settings.py
+++ asus/settings.py
@@ -25,7 +25,7 @@ environ.Env.read_env()
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET')
+SECRET_KEY = os.getenv('SECRET')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
