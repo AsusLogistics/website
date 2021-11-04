4ede9e83cf284fe6de830b3034fef755feacec49
asus <asuslogisticssocial@gmail.com>
2021-11-04 13:21:47 +0000

    major changes

--- AsusLogistic/views.py
+++ AsusLogistic/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import *
+from django.http import HttpResponse, JsonResponse, FileResponse, Http404
 
 # Create your views here.
+def Landing_Page(request):
+    return render(request, 'Landing Page.html')
