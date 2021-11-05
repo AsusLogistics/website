5a5f9e5e1e71aa59fc54be3416022c5bc99e832a
adil <adilgreet@gmail.com>
2021-11-05 11:25:33 +0000

    added paypal

--- asus/settings.py
+++ asus/settings.py
@@ -128,3 +128,6 @@ MEDIA_URL = '/media/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+PAYPAL_CLIENT_ID = ('PAYPAL_CLIENT_ID')
+PAYPAL_CLIENT_SECRET = ('PAYPAL_CLIENT_SECRET')
