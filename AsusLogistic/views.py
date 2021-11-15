ec8ab5323e8946b9cb4a64e8aa0fc7e74641b361
adil <adilgreet@gmail.com>
2021-11-15 10:51:56 +0000

    added comment

--- AsusLogistic/views.py
+++ AsusLogistic/views.py
@@ -13,6 +13,9 @@ def Booking_Page(request):
     form = BookingForm(request.POST)
     # check whether it's valid:
     if form.is_valid():
+        #variable
+        customer = form.save(commit=False)
+        customer.is_active = False
         form.save()
         # process the data in form.cleaned_data as required
         item_name = form.cleaned_data.get('item_name').lower()
