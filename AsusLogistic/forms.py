30b62c349461713e741878f17a8c68cee9458700
asus <asuslogisticssocial@gmail.com>
2021-11-15 11:45:55 +0000

    mistakes

--- AsusLogistic/forms.py
+++ AsusLogistic/forms.py
@@ -1,12 +1,16 @@
 from django import forms
 from .import models
 from django.forms import ModelForm
+<<<<<<< HEAD
 from .models import *
 from django.utils.safestring import mark_safe
+=======
+>>>>>>> parent of 4641f42... form works just won't save to database
 
 class BookingForm(forms.ModelForm):
 
     class Meta:
+<<<<<<< HEAD
         model = Item
         fields = ('item_name','item_width', 'item_height', 'item_length')
 
@@ -16,3 +20,7 @@ class BookingForm(forms.ModelForm):
            item = Item.objects.get(item_name=item_name)
         except Exception as e:
             return item_name
+=======
+        model = models.Item
+        fields = ['item_name', 'item_width', 'item_height', 'item_length']
+>>>>>>> parent of 4641f42... form works just won't save to database
--- AsusLogistic/models.py
+++ AsusLogistic/models.py
@@ -4,7 +4,8 @@ from django.utils import timezone
 from django.shortcuts import reverse
 from phonenumber_field.modelfields import PhoneNumberField
 from django.forms import ModelForm
-from django.contrib.auth.models import User
+
+
 
 
 # Create your models here.
@@ -14,10 +15,6 @@ class Item(models.Model):
     item_width = models.CharField(max_length=200, null=True)
     item_height = models.CharField(max_length=200, null=True)
     item_length = models.CharField(max_length=200, null=True)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-
-    def publish(self):
-        self.save()
 
     def __str__(self):
         return self.item_name
