9d31564b77aef0149fd2748a4605648f80d69331
adil <adilgreet@gmail.com>
2021-11-18 09:33:53 +0000

    added second form

--- AsusLogistic/views.py
+++ AsusLogistic/views.py
@@ -1,39 +1,42 @@
 from django.shortcuts import render
 from .models import *
 from .forms import *
-from django import forms
-from django.http import HttpResponseRedirect, JsonResponse, FileResponse, Http404
+from shapeshifter.views import MultiFormView
 
 # Create your views here.
 def Landing_Page(request):
-    return render(request, 'Landing Page.html')
+    return render(request, "Landing Page.html")
+
+context = {
+    'ItemForm': ItemForm(),
+    'SenderForm': SenderForm()
+}
 
 def Booking_Page(request):
-    # create a form instance and populate it with data from the request:
-    form = BookingForm(request.POST)
-    # check whether it's valid:
-    if form.is_valid():
-        customer = form.save(commit=False)
-        customer.is_active = False
-        form.save()
-        # process the data in form.cleaned_data as required
-        item_name = form.cleaned_data.get('item_name').lower()
-        # redirect to a new URL:
-        return HttpResponseRedirect('Landing Page')
-
-# if this is a POST request we need to process the form data
-    if request.method == 'POST':
-        # create a form instance and populate it with data from the request:
-        form = BookingForm(request.POST)
-        # check whether it's valid:
+    if request.method == "POST":
+        form = ItemForm(request.POST or None)
         if form.is_valid():
-            # process the data in form.cleaned_data as required
-            # ...
-            # redirect to a new URL:
-            return HttpResponseRedirect('Landing Page')
-
-        # if a GET (or any other method) we'll create a blank form
+            obj = Item() #gets new object
+            obj.item_name = form.cleaned_data['item_name']
+            obj.item_width = form.cleaned_data['item_width']
+            obj.item_height = form.cleaned_data['item_height']
+            obj.item_length = form.cleaned_data['item_length']
+            #finally save the object in db
+            obj.save()
     else:
-        form = BookingForm()
-        
-    return render(request, 'Booking.html', {'form': form})
+        form = ItemForm()
+    return render(request, "Booking.html", context)
+
+def Sender_details(request):
+    if request.method == "POST":
+        form2 = SenderForm(request.POST or None)
+        if form2.is_valid():
+            obj = Sender() #gets new object
+            obj.name = form2.cleaned_data['name']
+            obj.email = form2.cleaned_data['email']
+            obj.phone_number = form2.cleaned_data['phone_number']
+            #finally save the object in db
+            obj.save()
+    else:
+        form2 = SenderForm()
+    return render(request, "Booking.html", context)
